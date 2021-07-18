26bf339c90830ab147a199b7133a8c5aa3935f1e
Sami J. Mäkinen <sjm@iki.fi>
2021-07-18 15:58:34 +0300

    Migrate from Rocket to actix-web.

--- src/main.rs
+++ src/main.rs
@@ -2,47 +2,48 @@
 #![feature(once_cell)]
 
 extern crate coap;
-// extern crate log;
 extern crate parking_lot;
-#[macro_use]
-extern crate rocket;
-// extern crate simplelog;
 extern crate tera;
 
+use actix_web::{get, middleware, web, App, HttpResponse, HttpServer, Result};
 use chrono::*;
 use coap::CoAPClient;
-// use log::*;
+use log::*;
 use parking_lot::*;
-// use rocket::{Build, Rocket};
-use rocket_dyn_templates::Template;
-// use simplelog::*;
-use std::{lazy::*, time};
+use std::{env, lazy::*, time};
 use structopt::StructOpt;
-// use tera::Context;
+use tera::Tera;
+use actix_web::http::StatusCode;
 
 static COAP_URL: SyncLazy<RwLock<String>> = SyncLazy::new(|| RwLock::new(String::new()));
+static TERA_DIR: SyncLazy<RwLock<String>> = SyncLazy::new(|| RwLock::new(String::new()));
+static TERA: SyncLazy<RwLock<Tera>> =
+    SyncLazy::new(||
+        RwLock::new(match Tera::new(&format!("{}/*.tera", TERA_DIR.read())) {
+        Ok(t) => t,
+        Err(e) => {
+            eprintln!("Tera template parsing error: {}", e);
+            ::std::process::exit(1);
+        }
+    }));
 
 #[derive(Debug, StructOpt)]
-/// Note: internal InfluxDB client is used unless --influx-binary option is set.
 pub struct GlobalServerOptions {
-    #[structopt(short, long, default_value = "127.0.0.1")]
-    pub address: String,
-    #[structopt(short, long, default_value = "8080")]
-    pub port: u32,
-    #[structopt(short, long, default_value = "coap://127.0.0.1/")]
-    pub coap_url: String,
-    #[structopt(long, default_value = "templates")]
-    pub template_dir: String,
     #[structopt(short, long)]
     pub debug: bool,
     #[structopt(short, long)]
     pub trace: bool,
+    #[structopt(short, long, default_value = "127.0.0.1:8080")]
+    pub listen: String,
+    #[structopt(short, long, default_value = "coap://127.0.0.1/")]
+    pub coap_url: String,
+    #[structopt(long, default_value = "templates")]
+    pub template_dir: String,
 }
 
-#[rocket::main]
-async fn main() {
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
     let opt = GlobalServerOptions::from_args();
-    /*
     let loglevel = if opt.trace {
         LevelFilter::Trace
     } else if opt.debug {
@@ -50,27 +51,12 @@ async fn main() {
     } else {
         LevelFilter::Info
     };
-    SimpleLogger::init(
-        loglevel,
-        ConfigBuilder::new()
-            .set_time_format_str("%Y-%m-%d %H:%M:%S %Z")
-            .build(),
-    ).unwrap();
-    */
 
-    {
-        let mut u = COAP_URL.write();
-        *u = opt.coap_url.clone();
-    }
-    let config = rocket::Config::figment()
-        .merge(("address", &opt.address))
-        .merge(("port", opt.port))
-        .merge(("template_dir", &opt.template_dir))
-        .merge(("log_level", if opt.debug { "debug" } else { "normal" }));
-
-    let r = rocket::custom(config)
-        .mount("/", routes![index, cmd])
-        .attach(Template::fairing());
+    // env::set_var("RUST_LOG", "actix_web=debug,actix_server=info");
+    env_logger::Builder::new()
+        .filter_level(loglevel)
+        .format_timestamp_secs()
+        .init();
     info!(
         "pwr-server built from branch: {} commit: {}",
         env!("GIT_BRANCH"),
@@ -78,31 +64,53 @@ async fn main() {
     );
     info!("Source timestamp: {}", env!("SOURCE_TIMESTAMP"));
     info!("Compiler version: {}", env!("RUSTC_VERSION"));
-    let ret = r.launch().await;
-    match ret {
-        Ok(()) => {
-            info!("Program ended.");
-        }
+
+    {
+        let mut u = COAP_URL.write();
+        *u = opt.coap_url.clone();
+    }
+    {
+        let mut d = TERA_DIR.write();
+        *d = opt.template_dir.clone();
+    }
+    info!("Have templates: [{}]", TERA.read().get_template_names().collect::<Vec<_>>().join(", "));
+
+    HttpServer::new(|| App::new()
+        .wrap(middleware::Logger::default())
+        .service(cmd)
+        .route("/", web::get().to(index))
+        .route("/pwr/", web::get().to(index)))
+        .bind(&opt.listen)?
+        .run()
+        .await
+}
+
+#[allow(unused_mut)]
+async fn index() -> Result<HttpResponse>  {
+    let mut context = tera::Context::new();
+    // we could add variables for the template with context.insert() here
+    // but for now, we don't have any (:
+    match TERA.read().render("index.html.tera", &context) {
         Err(e) => {
-            error!("Program aborted: {:?}", e);
+            Ok(HttpResponse::build(StatusCode::INTERNAL_SERVER_ERROR)
+                   .content_type("text/plain")
+                   .body(format!("Template render error: {}", e)))
+        },
+        Ok(t) => {
+            Ok(HttpResponse::build(StatusCode::OK)
+                   .content_type("text/html; charset=utf-8")
+                   .body(t))
         }
     }
 }
 
-#[allow(unused_mut)]
-#[get("/")]
-fn index() -> Template {
-    let mut context = tera::Context::new();
-    // we could add variables for the template with context.insert() here
-    // but for now, we don't have any (:
-    Template::render("index", &context.into_json())
-}
-
-#[get("/cmd/<op>")]
-fn cmd(op: &str) -> String {
+#[get("/pwr/cmd/{op}")]
+async fn cmd(path: web::Path<(String,)>) -> Result<HttpResponse> {
+    let (op,) = path.into_inner();
     let url_pre = COAP_URL.read();
     let url;
     let coap_data = Utc::now().timestamp().to_string();
+
     if op.eq("on") {
         url = format!("{}{}", &url_pre, "pwr_on");
     } else if op.eq("off") {
@@ -113,26 +121,35 @@ fn cmd(op: &str) -> String {
     debug!("CoAP POST: {} <-- {}", &url, &coap_data);
     let res =
         CoAPClient::post_with_timeout(&url, coap_data.into_bytes(), time::Duration::from_secs(5));
+
     match res {
         Err(e) => {
-            format!("CoAP error: {:?}", e)
+            Ok(HttpResponse::build(StatusCode::INTERNAL_SERVER_ERROR)
+                   .content_type("text/plain")
+                   .body(format!("CoAP error: {:?}", e)))
         }
         Ok(resp) => {
             let msg = String::from_utf8_lossy(&resp.message.payload);
             debug!("CoAP reply: \"{}\"", &msg);
             let indata: Vec<&str> = msg.split(':').collect();
             if indata.len() != 2 {
-                return format!("CoAP error: invalid response: \"{}\"", &msg);
+                return Ok(HttpResponse::build(StatusCode::INTERNAL_SERVER_ERROR)
+                              .content_type("text/plain")
+                              .body(format!("CoAP error: invalid response: \"{}\"", &msg)));
             }
             let state = if indata[0].eq("0") { "OFF" } else { "ON" };
             match indata[1].parse::<i64>() {
                 Err(e) => {
-                    format!("CoAP error: {:?}", e)
+                    Ok(HttpResponse::build(StatusCode::INTERNAL_SERVER_ERROR)
+                           .content_type("text/plain")
+                           .body(format!("CoAP error: {:?}", e)))
                 }
                 Ok(changed) => {
                     let ts = NaiveDateTime::from_timestamp(changed, 0);
                     let ts_str = Local.from_utc_datetime(&ts).format("%Y-%m-%d %H:%M:%S %Z");
-                    format!("Power {}, last change: {}", state, ts_str)
+                    Ok(HttpResponse::build(StatusCode::OK)
+                           .content_type("text/plain")
+                           .body(format!("Power {}, last change: {}", state, ts_str)))
                 }
             }
         }
