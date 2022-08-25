105961f78fdf50249f324f8db8db10b239e54d11
Sami J. Mäkinen <sjm@iki.fi>
2022-08-25 10:07:10 +0300

    oops

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 export PATH
 
 tgt=$HOME/pwr-server/bin
-rsync -var target/release/pwr-server $tgt/
+rsync -var target/release/pwr_server $tgt/
 
 exit 0
 # EOF
